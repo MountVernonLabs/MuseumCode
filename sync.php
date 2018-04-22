ee9f21d1cd45b5af577db38bbde6f1bda8b8e6da
Matt Briney <mbriney@Matts-13-MacBook-Pro-2.local>
2018-04-21 20:11:36 -0400

    Sync script ready
    Next up:
    - Front end display using UIKit
    - Search and filtering

--- sync.php
+++ sync.php
@@ -14,19 +14,48 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo $row["user"]."\n";
+    echo "Looking at ".$row["user"]."\n";
 
-      // Get user profile from GitHub
+      // Get user profile from GitHub and update database entry
       $github_user = file_get_contents("http://api.github.com/users/".$row["user"], true, $context);
       $user = json_decode($github_user);
-      echo $user->{"id"}."\n";
 
-      $sql = "UPDATE organizations SET id=".$user->{"id"}." WHERE user='".$row["user"]."'";
-      if ($conn->query($sql) === TRUE) {
-          echo "Record updated successfully";
-      } else {
-          echo "Error updating record: " . $conn->error;
+      $sql = "UPDATE organizations SET id=".$user->{"id"}." WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET name='".$user->{"name"}."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET avatar='".$user->{"avatar_url"}."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET email='".$user->{"email"}."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET location='".$user->{"location"}."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET bio='".addslashes($user->{"bio"})."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET repos=".$user->{"public_repos"}." WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      $sql = "UPDATE organizations SET url='".$user->{"blog"}."' WHERE user='".$row["user"]."';";
+      $conn->query($sql);
+
+      // Get a list of repos for the organization
+      $github_repos = file_get_contents("http://api.github.com/users/".$row["user"]."/repos?per_page=100", true, $context);
+      $repos = json_decode($github_repos);
+
+      foreach ($repos as $repo){
+        echo "Found ".$repo->{'name'}."\n";
+
+        $sql = "INSERT INTO repos SET id=".$repo->{'id'}.",name='".$repo->{'name'}."',description='".addslashes($repo->{'description'})."',url='".$repo->{'html_url'}."',created='".$repo->{'created_at'}."',updated='".$repo->{'updated_at'}."',language='".$repo->{'language'}."' ON DUPLICATE KEY UPDATE name='".$repo->{'name'}."',description='".addslashes($repo->{'description'})."',url='".$repo->{'html_url'}."',created='".$repo->{'created_at'}."',updated='".$repo->{'updated_at'}."',language='".$repo->{'language'}."';";
+        $conn->query($sql);
+
       }
+      echo "\n\n";
   }
 }
 
